4c13d886e3a1608e9075c6b43279a4ec336437a9
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2021-07-10 18:07:44 -0500

    Update index.js

--- Example/src/index.js
+++ Example/src/index.js
@@ -1,14 +1,9 @@
 ajaxy("src/info.json","text");
 
-function output(data){
-
- data = JSON.parse(data)
-for(dat of data.cars){
-document.write(" Brand : "+dat.brand+"<br>" +" Year : "+dat.year+ "<br>"+" Speed : "+dat.speed +"<br><br><br>")
-}
-
-
-
-
+function output(resp){
+  data = JSON.parse(resp)
+  for(data of datas.cars){
+     document.write(" Brand : "+data.brand+"<br>" +" Year : "+data.year+ "<br>"+" Speed : "+data.speed +"<br><br><br>")
+  }
 
 }
