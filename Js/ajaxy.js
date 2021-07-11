48478dc9bc0446c390c8f422bf31783e09617840
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2021-07-10 22:01:11 -0500

    Update ajaxy.js

--- Js/ajaxy.js
+++ Js/ajaxy.js
@@ -1,1 +1,1 @@
-function ajaxy(e,t){xhr=new XMLHttpRequest,xhr.open("get",e),xhr.responseType=t,xhr.send(),xhr.onreadystatechange=function(){4==xhr.readyState&&(output(xhr.response),"text/xml"==t&&output(xhr.response))}}
+var self;function Ajaxy(e,t){xhr=new XMLHttpRequest,xhr.open("get",e),xhr.responseType=t,xhr.send(),xhr.onreadystatechange=function(){4==xhr.readyState&&(output=self.output(xhr.response),"text/xml"==t&&(output=self.output(xhr.response)))},self=this}
