e6a66eadee878830f08ab979d8011ddc5c7f9b74
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2021-08-19 21:48:38 -0500

    Update index.js

--- Example/src/index.js
+++ Example/src/index.js
@@ -5,7 +5,7 @@ var url ="src/info.json";
 var ajax = new Ajaxy(url,"text")
 
 ajax.output = function(resp){
- datas = JSON.parse(resp)
+ var datas = JSON.parse(resp)
 for(data of datas.cars){
 result.innerHTML += " Brand : "+data.brand+"<br>" +" Year : "+data.year+ "<br>"+" Speed : "+data.speed +"<br><br>"
 
