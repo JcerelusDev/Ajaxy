424435f2de8f8900cf5a37a43981df963c1a7944
JcerelusDev <54859793+JcerelusDev@users.noreply.github.com>
2022-09-30 16:48:15 -0500

    Update index.js

--- Example/src/index.js
+++ Example/src/index.js
@@ -2,14 +2,15 @@ var result = document.querySelector("#result");
 
 var url ="src/info.json";
 
-var ajax = new Ajaxy("get",url,"text")
+async function getCarInfo(){
+var resp = await Ajaxy("get",url)
+var data = await JSON.parse(resp)
 
-ajax.output = function(resp){
- var datas = JSON.parse(resp)
-for(var data of datas.cars){
-result.innerHTML += " Brand : "+data.brand+"<br>" +" Year : "+data.year+ "<br>"+" Speed : "+data.speed +"<br><br>"
+data.cars.forEach(function(car){
+text.innerHTML += 
+car.brand +"&nbsp; &nbsp;"+car.year+"&nbsp; &nbsp;"+car.speed+"<br/>"
+})
 
 }
 
-
-}
+getCarInfo()
